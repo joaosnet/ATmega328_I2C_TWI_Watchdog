275f4a39a9f797e87fa445a3c05d03f31d3098c0
João Natividade <87316339+joaosnet@users.noreply.github.com>
2023-12-08 21:46:55 +0000

    adicionando

---
+++ i2c/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 14/12/2022 17:54:19
+ * Author : isaia
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
